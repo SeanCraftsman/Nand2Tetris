767cb91a1714a3b589dcbc00476bdb04bd1dfff6
Sean <shironlai@sina.com>
2016-04-22 21:47:49 +0800

    fix conflict

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -3,34 +3,10 @@ import JackTokenizer
 import CompilationEngine
 import sys,os
 
-filename=sys.argv[1]
-readfile = open(filename,'r')
+filename = sys.argv[1]
+wfilename=filename.strip('.jack')+'.xml'
+tokenizer = JackTokenizer.Tokenizer(filename)
+outCompile = CompilationEngine.Compiler(tokenizer,wfilename)
 
-#clear all the // /* ... notes, create a new file to save the result
-copyfile = open('copyfile','w')
-line=readfile.readline()
-while line:
-	while line == '\n' or line.startswith('//'):
-		line=readfile.readline()
-	if '//' in line:
-		line=line[:line.find('//')]
-	if '/*' in line:
-		aline=line[:line.find('/*')]
-		while line.find('*/')<0:
-			line=readfile.readline()
-		bline=line[line.find('*/')+2:]
-		line=aline+bline
-	copyfile.write(line)
-	line=readfile.readline()
-copyfile.close()
-readfile.close()
 
-rfile=open('copyfile','r')
-wfile=open(filename.strip('.jack')+'.xml','w')
-
-outputCompile=CompilationEngine.Compile(rfile,wfile)
-outputCompile.compileClass()
-
-rfile.close()
-wfile.close()
 os.remove('copyfile')
