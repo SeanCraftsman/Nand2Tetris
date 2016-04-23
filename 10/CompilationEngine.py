8a478b653155dcb2fbe19d441cfaedc01cdf1b27
Sean <shironlai@sina.com>
2016-04-23 09:30:45 +0800

    Chapter 10

--- 10/CompilationEngine.py
+++ 10/CompilationEngine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import JackTokenizer
 
 class Compile():
--- 10/JackTokenizer.py
+++ 10/JackTokenizer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 STable=('{','}','(',')','[',']','.',',',';','+','-','*','/','&','|','<','>','=','~')
 KWtable=('class','constructor','function','method','field','static','var','int','char','boolean',\
 	'void','true','false','null','this','let','do','if','else','while','return')
